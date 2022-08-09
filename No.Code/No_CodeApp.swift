cb076ff3e7e4b4ffc4cc225d0ab9a87263ea70f7
Сафир Михаил Дмитриевич [B] <MDSafir@sberbank.ru>
2022-08-09 14:54:33 +0300

    Initial Commit

---
+++ No.Code/No_CodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  No_CodeApp.swift
+//  No.Code
+//
+//  Created by Сафир Михаил Дмитриевич [B] on 09.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct No_CodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
